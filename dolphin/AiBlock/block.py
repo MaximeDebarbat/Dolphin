03862ba8d9eb3d20e74990422ba11c63a506a6ab
maxime <maxime@smartcow.ai>
2023-02-19 15:09:36 +0100

    Created a general block definition

--- dolphin/AiBlock/block.py
+++ dolphin/AiBlock/block.py
@@ -1,7 +1,9 @@
 
+import pycuda.autoinit
+import pycuda.driver as cuda
+
 from typing import List, callable
-from AiBlock.TrtWrapper import TRTInference 
-from AiBlock.CudaUtils import bindings
+from AiBlock.TrtWrapper import TRTInference
 from AiBlock import TransitData
 
 class Block(object):
@@ -10,13 +12,35 @@ class Block(object):
                  Preprocessors:List[callable],
                  Postprocessors:List[callable],
                  TrtEngine:TRTInference,
-                 cuda_bindings:bindings):
+                 stream:cuda.Stream | None = None):
         
         self._Preprocessors = Preprocessors
         self._Postprocessors = Postprocessors
         self._TrtEngine = TrtEngine
-        self._cuda_bindings = cuda_bindings
+        if(stream is None):
+            self._stream = cuda.Stream()
+        else:
+            self._stream = stream
     
-    def __call__(self, input:TransitData) -> TransitData:
+    def __call__(self, input:object) -> TransitData:
         
+        x = input
+        for __prp in self._Preprocessors:
+            x = __prp(x, stream=self._stream)
+            
+        self._stream.synchronize()
         
+        x = self._TrtEngine(x, stream=self._stream)
+        
+        self._stream.synchronize()
+        
+        for __ptp in self._Postprocessors:
+            x = __ptp(x, stream=self._stream)
+        
+        self._stream.synchronize()
+            
+        return x
+    
+    @property
+    def stream(self):
+        return self._stream
