971cf9446f85756d1293f10459926201c73f8ab9
MaximeDebarbat <maxime@smartcow.ai>
2023-02-24 19:28:48 +0100

    Trying to fix cuda resize INTER_NEAREST

--- dolphin/AiBlock/processor/cuda/resize.cu
+++ dolphin/AiBlock/processor/cuda/resize.cu
@@ -6,16 +6,23 @@ struct ImageSize {
     uint16_t channels;
 };
 
-__global__ void resize(uint16_t *src_img, 
-                       uint16_t *dst_img,
+__global__ void resize(uint8_t *src_img, 
+                       uint8_t *dst_img,
                        ImageSize *src_image_size,
                        ImageSize *dst_image_size){
+
+    /*
+    src_img and dst_img are expected to be HWC images     
+    */
+    //printf("src_image_size : (%d,%d,%d) dst_image_size : (%d,%d,%d)\n",src_image_size->width,src_image_size->height,src_image_size->channels,dst_image_size->width,dst_image_size->height,dst_image_size->channels);
 
     uint16_t i = blockDim.x * blockIdx.x + threadIdx.x;
     uint16_t j = blockDim.y * blockIdx.y + threadIdx.y;
 
-    if (i < (*dst_w) && j < (*dst_h)) 
+    if (i < (dst_image_size->width) && j < (dst_image_size->height)) 
     {
+
+        //printf("(%d,%d) \n",i, j);
         uint32_t iIn = (uint32_t) (i * (src_image_size->width) / (dst_image_size->width));
         uint32_t jIn = (uint32_t) (j * (src_image_size->height) / (dst_image_size->height));
 
@@ -24,6 +31,7 @@ __global__ void resize(uint16_t *src_img,
 
         for (uint32_t c = 0; c < src_image_size->channels; ++c) 
         { 
+            //printf("(%d,%d) -> %d\n",i,j,src_img[(uint32_t) src_offset + c]);
             dst_img[(uint32_t) dst_offset + c] = src_img[(uint32_t) src_offset + c];
         }
     }
--- dolphin/AiBlock/processor/resize.py
+++ dolphin/AiBlock/processor/resize.py
@@ -1,17 +1,19 @@
 import pycuda.autoinit
 import pycuda.driver as cuda
-import numpy as np
 
 from pycuda.compiler import SourceModule
 
-from enum import Enum
+import numpy as np
+
 import os
 import sys
-import inspect
+sys.path.append("..")
+sys.path.append("../..")
+
 import math
 
 from CudaUtils import CUDA_BASE, CUDA_Binding
-from Data import ImageSize, BoundingBox
+from Data import ImageSize
 
 class CuResize(CUDA_BASE):
     
@@ -35,26 +37,68 @@ class CuResize(CUDA_BASE):
         self._out_image_size_binding.write(data=out_image_size.ndarray)
         self._out_image_size_binding.H2D()
         
-        self._BLOCK = (self.MAX_BLOCK_X,self.MAX_BLOCK_Y,1)        
+        self._BLOCK = (min(self._out_image_size.width,self.MAX_BLOCK_X),min(self._out_image_size.height,self.MAX_BLOCK_Y),1)        
+        self._GRID = (max(1,math.ceil(self._out_image_size.width/self._BLOCK[0])),max(1,math.ceil(self._out_image_size.height/self._BLOCK[1])))
         
     def __call__(self,
                  in_image_binding:CUDA_Binding,
                  in_image_size_binding:CUDA_Binding,
                  out_image_binding:CUDA_Binding,
-                 stream:cuda.Stream
+                 stream:cuda.Stream=None
                 )->None:
         
-        _T =  math.ceil(in_image_size_binding.size/self.TOTAL_THREADS)
-        _GRID = (_T,_T)
+        
+        
+        #print(f"block : {self._BLOCK} {self._GRID}")
+        #sys.exit(0)
         
         self._RESIZE_CUDA_F(in_image_binding.device,
                             out_image_binding.device,
                             in_image_size_binding.device,
                             self._out_image_size_binding.device,
-                            block=self._BLOCK, grid=_GRID,
+                            block=self._BLOCK, grid=self._GRID,
                             stream=stream
                             )
         
     
+if __name__ == "__main__":
+    
+    import time
+    import cv2
+    
+    stream = cuda.Stream()
+    
+    out_image_size = ImageSize(width=100, height=100, channels=3, dtype=np.uint8)
+    resizer = CuResize(out_image_size=out_image_size)
+    
+    image = cv2.imread("dog.jpg")
         
+    in_image_binding = CUDA_Binding()
+    in_image_binding.allocate(shape=(image.shape[0],image.shape[1],3), dtype=np.uint8)
+    in_image_binding.write(data=image.flatten(order="C")) 
+    in_image_binding.H2D(stream=stream)
+    
+    in_image_size_binding = CUDA_Binding()
+    in_image_size_binding.allocate(shape=(3,), dtype=np.uint16)
+    in_image_size_binding.write(np.array(image.shape))
+    in_image_size_binding.H2D(stream=stream)
+    
+    out_image_binding = CUDA_Binding()
+    out_image_binding.allocate(shape=(out_image_size.height,out_image_size.width,out_image_size.channels), dtype=np.uint8)
+    
+    N_ITER = int(1e5)
+    t1 = time.time()    
+    for _ in range(N_ITER):
+        resizer(in_image_binding=in_image_binding,
+                in_image_size_binding=in_image_size_binding,
+                out_image_binding=out_image_binding,
+                stream=stream)
+    
+    print(f"AVG CUDA Time : {1000/N_ITER*(time.time()-t1)}")
+    
+    out_image_binding.D2H(stream=stream)    
         
+    print(f"Original image :\n{in_image_binding.value}")
+    print(f"Reshaped image :\n{out_image_binding.value}")
+    
+    cv2.imwrite("resized.jpg", out_image_binding.value)
