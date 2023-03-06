66fb31f5384f86eec26eb5ff2bf7825e65c17fbf
MaximeDebarbat <debarbat.maxime@gmail.com>
2023-03-06 22:47:20 +0000

    Added CUDA implementation for 3 norm types : 255, 128 & Imagenet. Need to check imagenet one

--- dolphin/AiBlock/processor/cuda/normalize.cu
+++ dolphin/AiBlock/processor/cuda/normalize.cu
@@ -7,9 +7,11 @@ struct ImageSize {
     uint16_t channels;
 };
 
-__global__ void normalize(uint8_t *src_img,
+__global__ void normalize_255(uint8_t *src_img,
                           float *dst_img,
-                          ImageSize *image_size){
+                          ImageSize *image_size,
+                          float* mean,
+                          float* std){
 
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
@@ -21,6 +23,50 @@ __global__ void normalize(uint8_t *src_img,
         for (uint16_t c = 0; c < image_size->channels; c++){
 
             dst_img[offset + c] = (float) src_img[offset + c] / 255.0;
+
+        }
+    }
+
+}
+
+__global__ void normalize_128(uint8_t *src_img,
+                          float *dst_img,
+                          ImageSize *image_size,
+                          float* mean,
+                          float* std){
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (i < image_size->width && j < image_size->height) {
+        //printf("width, height : %d, %d \n", image_size->width, image_size->height);
+        uint32_t offset = (j * image_size->width + i) * image_size->channels;
+
+        for (uint16_t c = 0; c < image_size->channels; c++){
+
+            dst_img[offset + c] = (float) ((float) src_img[offset + c] - 128.0) / 128.0;
+
+        }
+    }
+
+}
+
+__global__ void normalize_mean_std(uint8_t *src_img,
+                          float *dst_img,
+                          ImageSize *image_size,
+                          float* mean,
+                          float* std){
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (i < image_size->width && j < image_size->height) {
+
+        uint32_t offset = (j * image_size->width + i) * image_size->channels;
+
+        for (uint16_t c = 0; c < image_size->channels; c++){
+
+            dst_img[offset + c] = (float) ((float) (src_img[offset + c] / 255.0) - mean[c] ) / std[c];
 
         }
     }
