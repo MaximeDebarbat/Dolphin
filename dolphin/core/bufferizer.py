9c814165bc60476ce29474ff07dc104ed0b02da7
MaximeDebarbat <debarbat.maxime@gmail.com>
2023-04-22 20:43:17 +0000

    Moved bufferizer & trtbuffer into core/

---
+++ dolphin/core/bufferizer.py
@@ -0,0 +1,367 @@
+"""_summary_
+"""
+
+import pycuda.driver as cuda  # pylint: disable=import-error
+import tensorrt as trt  # pylint: disable=import-error
+
+from typing import Any
+import dolphin
+
+
+class Bufferizer:
+    """
+    # Bufferizer
+
+    Bufferizer is a class that allows to easily bufferize data on the GPU.
+    The purpose is to handle seamlessly batched data and to avoid unnecessary
+    memory allocation but rather reuse the same memory buffer and favour
+    copy operations.
+
+
+    :param shape: shape of element to bufferize
+    :type shape: tuple
+    :param buffer_size: size of the buffer
+    :type buffer_size: int
+    :param dtype: dtype of the element to bufferize
+    :type dtype: dolphin.dtype
+    :param stream: stream to use for the buffer, defaults to None
+    :type stream: cuda.Stream, optional
+    :param flush_hook: callable triggered when buffer is flushed, defaults to None
+    :type flush_hook: callable, optional
+    :param allocate_hook: callable triggered when buffer is allocated, defaults to None
+    :type allocate_hook: callable, optional
+    :param append_one_hook: callable triggered when buffer has a new element appended, defaults to None
+    :type append_one_hook: callable, optional
+    :param append_multiple_hook: callable triggered when buffer has new elements appended, defaults to None
+    :type append_multiple_hook: callable, optional
+    :param buffer_full_hook: callable triggered when the buffer is full after calling any append, defaults to None
+    :type buffer_full_hook: callable, optional
+    """
+
+    def __init__(self,
+                 shape: tuple,
+                 buffer_size: int,
+                 dtype: dolphin.dtype,
+                 stream: cuda.Stream = None,
+                 flush_hook: callable = None,
+                 allocate_hook: callable = None,
+                 append_one_hook: callable = None,
+                 append_multiple_hook: callable = None,
+                 buffer_full_hook: callable = None
+                 ):
+        """
+        Constructor of the `Bufferizer` class.
+        """
+
+        self._shape: tuple = shape
+        self._dtype: dolphin.dtype = dtype
+        self._buffer_len: int = buffer_size
+        self._itemsize: int = trt.volume(self._shape)
+        self._nbytes: int = self._buffer_len * self._itemsize * self._dtype.itemsize
+        self._n_elements: int = 0
+        self._allocation: cuda.DeviceAllocation = None
+        self._stream: cuda.Stream = stream
+
+        self._flush_hook: callable = None
+        self._allocate_hook: callable = None
+        self._append_one_hook: callable = None
+        self._append_multiple_hook: callable = None
+        self._buffer_full_hook: callable = None
+
+        self.allocate()
+
+        self._flush_hook = flush_hook
+        self._allocate_hook = allocate_hook
+        self._append_one_hook = append_one_hook
+        self._append_multiple_hook = append_multiple_hook
+        self._buffer_full_hook = buffer_full_hook
+
+    def allocate(self) -> None:
+        """
+        Method to allocate the buffer on the GPU.
+        This method is called automatically when the class is instanciated.
+
+        Once the buffer is allocated, it is not possible to change the size
+        and it the allocation is initialized to 0.
+
+        Also, this methods triggers the allocate_hook if it is not None.
+        """
+
+        self._allocation = cuda.mem_alloc(self._buffer_len * self._itemsize
+                                          * self._dtype.itemsize)
+        self._n_elements = 0
+
+        if self._allocate_hook is not None:
+            self._allocate_hook(self)
+
+        self.flush(0)
+
+    def append_one(self, element: dolphin.darray) -> None:
+        # pylint: disable=no-member
+        """
+        Method to append one element to the buffer.
+        The element is copied and appended to the buffer.
+        `element` must be a `darray` of the same shape and dtype as the
+        `bufferizer`.
+        The size of the buffer is increased by one.
+
+        Appending one element triggers the `append_one_hook` if it is not None.
+        Once the buffer is full, the `buffer_full_hook` is triggered.
+
+        :param element: The element to append to the buffer.
+        :type element: dolphin.darray
+        """
+
+        if self._n_elements == self._buffer_len:
+            raise BufferError(
+                f"Bufferizer is full ({self._buffer_len} elements).")
+
+        if element.shape != self._shape:
+            raise ValueError(f"Element shape {element.shape} does not match \
+                              bufferizer shape {self._shape}.")
+
+        if element.dtype != self._dtype:
+            raise ValueError(f"Element dtype {element.dtype} does not match \
+                              bufferizer dtype {self._dtype}.")
+
+        cuda.memcpy_dtod_async(int(self._allocation) + self._n_elements
+                               * self._itemsize,
+                               element.allocation,
+                               self._itemsize *
+                               self._dtype.itemsize,
+                               self._stream)
+
+        self._n_elements += 1
+
+        if self._append_one_hook is not None:
+            self._append_one_hook(self)
+
+        if self.full:
+            if self._buffer_full_hook is not None:
+                self._buffer_full_hook(self)
+
+    def append_multiple(self, element: dolphin.darray) -> None:
+        # pylint: disable=no-member
+        """
+        Function used in order to append multiple `darray` to the buffer
+        at once. The `darray` must be of the same shape and dtype as the
+        `bufferizer`.
+
+        The size of the buffer is increased by the number of elements in
+        `element`.
+
+        It is assumed that the number of elements in `element` is defined
+        as the first dimension of its shape.
+        For instance::
+
+            element.shape = (batch_size, *self._shape)
+
+        Appending multiple elements triggers the `append_multiple_hook` if it
+        is not None. Once the buffer is full, the `buffer_full_hook` is
+        triggered if it is not None.
+
+        :param element: The element to append to the buffer.
+        :type element: darray
+        """
+        batch_size = element.shape[0]
+
+        if element.shape[1:] != self._shape:
+            raise ValueError(f"Element shape {element.shape} does not match \
+                              bufferizer shape {self._shape}.")
+
+        if self._n_elements == self._buffer_len:
+            raise BufferError(f"Bufferizer is full (max \
+                                {self._buffer_len} elements).")
+
+        if self._n_elements + batch_size > self._buffer_len:
+            raise BufferError(f"Bufferizer is going to be overflowed \
+                ({self._buffer_len} elements). Tried to push \
+                {batch_size} elements.")
+
+        cuda.memcpy_dtod_async(int(self._allocation) + self._n_elements
+                               * self._itemsize,
+                               element.allocation,
+                               batch_size * self._itemsize *
+                               self._dtype.itemsize,
+                               self._stream)  # pylint: disable=no-member
+
+        self._n_elements += batch_size
+
+        if self._append_multiple_hook is not None:
+            self._append_multiple_hook(self)
+
+        if self.full:
+            if self._buffer_full_hook is not None:
+                self._buffer_full_hook(self)
+
+    def flush(self, value: Any = 0) -> None:
+        # pylint: disable=no-member
+        """
+        Set the buffer to a given value.
+        Useful in order to get rid of any residual data in the buffer.
+
+        Calling this method triggers the `flush_hook` if it is not None.
+
+        :param stream: Cuda stream, defaults to None
+        :type stream: cuda.Stream, optional
+        """
+
+        self.darray.fill(value)
+
+        # pylint: disable=no-member
+
+        self._n_elements = 0
+
+        if self._flush_hook is not None:
+            self._flush_hook(self)
+
+    def flush_hook(self, hook: callable):
+        """
+        Method to set the flush hook.
+        This hook is called when the `flush` method is called.
+
+        :param hook: Callable function called each time the `flush` method is
+                        called.
+        :type hook: callable
+        """
+        self._flush_hook = hook
+
+    def allocate_hook(self, hook: callable):
+        """
+        Method to set the allocate hook.
+        This hook is called when the `allocate` method is called.
+
+        :param hook: Callable function called each time the `allocate` method
+                        is called.
+        :type hook: callable
+        """
+        self._allocate_hook = hook
+
+    def append_one_hook(self, hook: callable):
+        """
+        Method to set the append one hook.
+
+        :param hook: Callable function called each time the `append_one`
+                        method is called.
+        :type hook: callable
+        """
+        self._append_one_hook = hook
+
+    def append_multiple_hook(self, hook: callable):
+        """
+        Method to set the append multiple hook.
+
+        :param hook: Callable function called each time the
+                        `append_multiple` method is called.
+        :type hook: callable
+        """
+        self._append_multiple_hook = hook
+
+    def buffer_full_hook(self, hook: callable):
+        """
+        Method to set the buffer full hook.
+
+        :param hook: Callable function called each time the buffer is full.
+        :type hook: callable
+        """
+        self._buffer_full_hook = hook
+
+    @property
+    def allocation(self) -> cuda.DeviceAllocation:
+        """
+        Property in order to get the allocation of the buffer.
+
+        :return: Allocation of the buffer.
+        :rtype: cuda.DeviceAllocation
+        """
+        return self._allocation
+
+    @property
+    def darray(self) -> dolphin.darray:
+        """
+        Property in order to convert a bufferizer to a `darray`.
+
+        Important note : The `darray` returned by this property is not a copy
+        of the bufferizer. It is a view of the bufferizer. Any change to the
+        `darray` will be reflected in the bufferizer and vice-versa.
+
+        :return: darray of bufferizer
+        :rtype: dolphin.darray
+        """
+
+        res = dolphin.darray(shape=(self._buffer_len,) + self._shape,
+                             dtype=self._dtype,
+                             allocation=self._allocation,
+                             stream=self._stream)
+        return res
+
+    @property
+    def element_nbytes(self) -> int:
+        """
+        Property in order to get the number of bytes of a single element in
+        the buffer.
+
+        :return: Number of bytes of a single element in the buffer.
+        :rtype: int
+        """
+        return self._itemsize
+
+    @property
+    def nbytes(self) -> int:
+        """
+        Property in order to get the number of bytes of the buffer.
+
+        :return: Number of bytes of the buffer.
+        :rtype: int
+        """
+        return self._nbytes
+
+    @property
+    def full(self) -> bool:
+        """
+        Property in order to know if the buffer is full.
+
+        :return: True if the buffer is full, False otherwise.
+        :rtype: bool
+        """
+        return self._n_elements == self._buffer_len
+
+    @property
+    def shape(self) -> tuple:
+        """
+        Property in order to get the shape of the the buffer.
+
+        :return: Shape of the buffer.
+        :rtype: tuple
+        """
+        return (self._buffer_len,) + self._shape
+
+    @property
+    def element_shape(self) -> tuple:
+        """
+        Property in order to get the shape of a single element in the buffer.
+
+        :return: Shape of a single element in the buffer.
+        :rtype: tuple
+        """
+        return self._shape
+
+    @property
+    def dtype(self) -> dolphin.dtype:
+        """
+        Property in order to get the dtype of the buffer.
+
+        :return: Dtype of the buffer.
+        :rtype: dolphin.dtype
+        """
+        return self._dtype
+
+    def __len__(self) -> int:
+        """
+        Method in order to get the number of elements in the buffer.
+
+        :return: Number of elements in the buffer.
+        :rtype: int
+        """
+
+        return self._n_elements
---
+++ dolphin/core/trtbuffer.py
@@ -0,0 +1,271 @@
+"""
+_summary_
+"""
+
+import pycuda.driver as cuda  # pylint: disable=import-error
+
+from typing import Any, Dict, List
+import dolphin
+
+
+class CudaTrtBuffers:
+    """
+    To be used with the `dolphin.engine` class.
+    This class actually manages the buffers used by the engine,
+    both for inputs and outputs.
+
+    To ease the use of the buffers, this class can be understood as a
+    dict in order to name inputs and outputs.
+
+    Note that the names of inputs and outputs have to match the names
+    of the inputs and outputs of the engine.
+
+    The constructor of this class takes an optional `cuda.Stream`
+    as an argument.
+    """
+
+    def __init__(self, stream: cuda.Stream = None):
+        """
+        Constructor of the class.
+        """
+
+        self._inputs: Dict[str, dolphin.Bufferizer] = {}
+        self._outputs: Dict[str, dolphin.Bufferizer] = {}
+
+        self._input_order = []
+        self._output_order = []
+
+        self._stream = stream
+
+    def allocate_input(self, name: str,
+                       shape: tuple,
+                       buffer_size: int,
+                       dtype: object,
+                       buffer_full_hook: callable = None,
+                       flush_hook: callable = None,
+                       allocate_hook: callable = None,
+                       append_one_hook: callable = None,
+                       append_multiple_hook: callable = None) -> None:
+        """
+        Method to allocate an input buffer.
+        This methods creates a `dolphin.Bufferizer` and adds it to the inputs
+        dict with the given name.
+
+        :param name: Name of the input.
+        :type name: str
+        :param shape: Shape of a single element in the buffer.
+        :type shape: tuple
+        :param buffer_size: Size of the buffer.
+        :type buffer_size: int
+        :param dtype: Dtype of the buffer.
+        :type dtype: object
+        :param buffer_full_hook: callable function called each time the
+                                    buffer is full.
+        :type buffer_full_hook: callable
+        :param flush_hook: callable function called each time the buffer
+                            is flushed.
+        :type flush_hook: callable
+        :param allocate_hook: callable function called each time the
+                                `allocate` method is called.
+        :type allocate_hook: callable
+        :param append_one_hook: callable function called each time the
+                                `append_one` method is called.
+        :type append_one_hook: callable
+        :param append_multiple_hook: callable function called each time the
+                                        `append_multiple` method is called.
+        :type append_multiple_hook: callable
+        """
+
+        self._inputs[name] = dolphin.Bufferizer(
+            shape=shape,
+            buffer_size=buffer_size,
+            dtype=dtype,
+            stream=self._stream,
+            buffer_full_hook=buffer_full_hook,
+            flush_hook=flush_hook,
+            allocate_hook=allocate_hook,
+            append_one_hook=append_one_hook,
+            append_multiple_hook=append_multiple_hook)
+        self._inputs[name].allocate()
+
+        self._input_order.append(name)
+
+    def allocate_output(self, name: str,
+                        shape: tuple,
+                        dtype: dolphin.dtype) -> None:
+        """
+        Method to allocate an output buffer.
+        Oppositely to the inputs, the outputs are not `dolphin.Bufferizer`.
+        They are `darray`.
+
+        :param name: Name of the output.
+        :type name: str
+        :param shape: Shape of a single element in the buffer.
+        :type shape: tuple
+        :param dtype: Dtype of the buffer.
+        :type dtype: dolphin.dtype
+        """
+
+        self._outputs[name] = dolphin.darray(shape=shape,
+                                             dtype=dtype,
+                                             stream=self._stream)
+
+        self._output_order.append(name)
+
+    def flush(self, value: Any = 0) -> None:
+        """
+        Method to flush all the input buffers.
+        Note that this method will trigger the `flush_hook` of each
+        `dolphin.Bufferizer`.
+
+        :param value: value to initialize the inputs with, defaults to 0
+        :type value: int, optional
+        """
+
+        for inp in self._inputs.values():
+            inp.flush(value)
+
+    def append_one_input(self, name: str,
+                         data: dolphin.darray):
+        """
+        Method to append a single element to the input buffer.
+        Note that this method will trigger the `append_one_hook` of the
+        `dolphin.Bufferizer`. It will also trigger the `buffer_full_hook`
+        if the buffer is full.
+
+        :param name: Name of the input.
+        :type name: str
+        :param data: Data to append.
+        :type data: CudaBinding
+        """
+
+        self._inputs[name].append_one(data)
+
+    def append_multiple_input(self, name: str,
+                              data: dolphin.darray):
+        """
+        Method to append multiple elements to the input buffer.
+        Note that this method will trigger the `append_multiple_hook` of the
+        `dolphin.Bufferizer`. It will also trigger the `buffer_full_hook`
+        if the buffer is full.
+
+        :param name: Name of the input.
+        :type name: str
+        :param data: Data to append.
+        :type data: CudaBinding
+        """
+
+        self._inputs[name].append_multiple_input(data)
+
+    @property
+    def input_shape(self) -> Dict[str, tuple]:
+        """
+        Property to get the shape of the inputs.
+        Returns a dict with the name of the input as key and the shape as
+        value.
+
+        :return: Shape of the inputs.
+        :rtype: Dict[str, tuple]
+        """
+        return {key: inp.shape for key, inp in self._inputs.items()}
+
+    @property
+    def input_dtype(self) -> Dict[str, dolphin.dtype]:
+        """
+        Property to get the dtype of the inputs.
+        Returns a dict with the name of the input as key and the dtype as
+        value.
+
+        :return: Dtype of the inputs.
+        :rtype: Dict[str, dolphin.dtype]
+        """
+        return {key: inp.dtype for key, inp in self._inputs.items()}
+
+    @property
+    def output_shape(self) -> Dict[str, tuple]:
+        """
+        Property to get the shape of the outputs.
+        Returns a dict with the name of the output as key and the shape as
+        value.
+
+        :return: Shape of the outputs.
+        :rtype: Dict[str, tuple]
+        """
+        return {key: out.shape for key, out in self._outputs.items()}
+
+    @property
+    def output_dtype(self):
+        """
+        Property to get the dtype of the outputs.
+        Returns a dict with the name of the output as key and the dtype as
+        value.
+
+        :return: Dtype of the outputs.
+        :rtype: Dict[str, dolphin.dtype]
+        """
+        return {key: out.dtype for key, out in self._outputs.items()}
+
+    @property
+    def full(self) -> bool:
+        """
+        Property to check if the buffer is full.
+        Returns True if at least one of the input buffer is full.
+
+        :return: True if at least one of the input buffer is full.
+        :rtype: bool
+        """
+
+        for binding_name in self._inputs.keys():
+            if self._inputs[binding_name].full:
+                return True
+
+        return False
+
+    @property
+    def output(self) -> Dict[str, dolphin.darray]:
+        """
+        Property to get the output of the buffer.
+        Returns a dict with the name of the output as key and the output as
+        value.
+
+        :return: Output of the buffer.
+        :rtype: Dict[str, dolphin.darray]
+        """
+
+        return {key: out for key, out in self._outputs.items()}
+
+    @property
+    def input_bindings(self) -> List[dolphin.darray]:
+        """
+        Property to get the input bindings.
+        'input bindings' refers here to the list
+        of input allocations
+
+        :return: Input bindings.
+        :rtype: List[dolphin.darray]
+        """
+
+        return [self._inputs[name].allocation for name in self._input_order]
+
+    @property
+    def output_bindings(self) -> List[int]:
+        """
+        Property to get the output bindings.
+        'output bindings' refers here to the list
+        of output allocations
+
+        :return: Output bindings.
+        :rtype: List[int]
+        """
+
+        return [self._outputs[name].allocation for name in self._output_order]
+
+    @property
+    def bindings(self):
+        """
+        Property to get the bindings.
+
+        :return: Bindings.
+        :rtype: List[int]
+        """
+        return self.input_bindings + self.output_bindings
