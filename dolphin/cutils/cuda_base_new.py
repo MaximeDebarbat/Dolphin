d66de178fd3386ddd216b1fff388fc52af42df57
MaximeDebarbat <debarbat.maxime@gmail.com>
2023-04-22 20:44:40 +0000

    Created a newer version of cuda_base that computes 1D block & grid better

--- dolphin/cutils/cuda_base_new.py
+++ dolphin/cutils/cuda_base_new.py
@@ -1,11 +1,12 @@
 
+from typing import Tuple
 import pycuda
 
 import numpy as np
 import math
 
 
-class CudaBase:
+class CudaBaseNew:
 
     def __init__(self):
 
@@ -36,19 +37,33 @@ class CudaBase:
         self.THREADS_BLOCKS_PER_MP = int(
             pycuda.tools.DeviceData(self.DEVICE).thread_blocks_per_mp)
 
-        if round(
-            np.sqrt(
-                self.MAX_THREADS_PER_BLOCKS)) != np.sqrt(
-                self.MAX_THREADS_PER_BLOCKS):
-            self.MAX_BLOCK_X = round(np.sqrt(self.MAX_THREADS_PER_BLOCKS))
-            self.MAX_BLOCK_Y = int(
-                self.MAX_THREADS_PER_BLOCKS /
-                self.MAX_BLOCK_X)
-        else:
-            self.MAX_BLOCK_X = int(np.sqrt(self.MAX_THREADS_PER_BLOCKS))
-            self.MAX_BLOCK_Y = int(np.sqrt(self.MAX_THREADS_PER_BLOCKS))
+        self.MIN_THREADS = self.WARP_SIZE
+        self.MAX_THREADS = 4*self.WARP_SIZE
 
-        self.TOTAL_THREADS = self.MAX_BLOCK_X * self.MAX_BLOCK_Y
+        self.MAX_BLOCKS = 4 * self.THREADS_BLOCKS_PER_MP * self.MULTIPROCESSOR_COUNT
+
+
+    def GET_BLOCK_GRID_1D(self, n: int) -> Tuple[tuple, tuple]:
+        """
+        In ordert to perform memory coalescing on 1D iterations,
+        we need to efficiently compute the block & grid sizes.
+
+        :param n: Number of elements to process
+        :type n: int
+        :return: block, grid
+        :rtype: Tuple[tuple, tuple]
+        """
+
+        if n < self.MIN_THREADS:
+            return (self.MIN_THREADS, 1, 1), (1, 1)
+        elif n < (self.MAX_BLOCKS * self.MIN_THREADS):
+            return (self.MIN_THREADS, 1, 1), ((n + self.MIN_THREADS - 1) // self.MIN_THREADS, 1)
+        elif n < (self.MAX_BLOCKS * self.MAX_THREADS):
+            grid = (self.MAX_BLOCKS, 1)
+            grp = (n + self.MIN_THREADS - 1) // self.MIN_THREADS
+            return ((grp + self.MAX_BLOCKS - 1) // self.MAX_BLOCKS * self.MIN_THREADS), grid
+
+        return (self.MAX_THREADS, 1, 1), (self.MAX_BLOCKS, 1)
 
     def _GET_BLOCK_X_Y(self, Z: int) -> tuple:
         """Get the block size for a given Z.
